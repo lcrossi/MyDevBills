7e49545856aba624876499b08ca643d412c2039c
Luis Rossi <lcrossi2010@gmail.com>
2022-07-27 11:23:07 -0300

    feat (initial page): beginning changes on first page

--- App.js
+++ App.js
@@ -6,107 +6,73 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React from 'react';
+ import {
+   Button,
+   FlatList,
+   StyleSheet,
+   Text,
+   View,
+ } from 'react-native';
+ 
+ const App = () => {
+ 
+   const addEntry = () => {
+     alert("Abrir tela de add lançamento")
+   }
+ 
+   return (
+     <View style={{padding: 10}}>
+       <Text
+         style={{
+           fontSize: 22,
+           fontWeight: "bold",
+           marginTop: 10, 
+           marginBottom:10
+         }}>Saldo: $2.102.53</Text>
+       <Button 
+         onPress={addEntry} 
+         title="Adicionar"></Button>
+       <Text
+       style={{
+         fontSize: 22,
+         fontWeight: "bold",
+         marginTop: 10, 
+         marginBottom:10
+       }}>Categorias</Text>
+ 
+       <FlatList
+         data={[
+           {key: "Alimentação: $ 200,00"},
+           {key: "Combustível: $ 12,00"},
+           {key: "Aluguel: $ 120,00"},
+           {key: "Lazer: $ 250,00"},
+           {key: "Outros: $ 1200,00"},
+         ]}
+         renderItem={({item}) => <Text>{item.key}</Text>}></FlatList>
+ 
+       <Text
+       style={{
+         fontSize: 22,
+         fontWeight: "bold",
+         marginTop: 10, 
+         marginBottom:10
+       }}>Últimos lançamentos</Text>
+ 
+       <FlatList
+         data={[
+           {key: "Padaria Asa Branca: $ 15,00"},
+           {key: "Sonda Supermercado: $ 80,00"},
+           {key: "Posto Ipiranga: $ 12,00"},
+         ]}
+         renderItem={({item}) => <Text>{item.key}</Text>}></FlatList>
+     </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+ 
+ });
+ 
+ export default App;
+ 
